18f43c9a4775c42133eb1f80f9749120fc67c1de
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-21 14:17:06 -0300

    -- Criando migrations -- Definindo Model de usuários corretamente -- Definindo um Seed para criação de usuários.

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // add 3 users to the database
+        for ($i = 1; $i <= 3; $i++) {
+            User::create([
+                'username' => "user$i",
+                'email' => "user$i@gmail.com",
+                'password' => bcrypt('Aa123456'),
+                'email_verified_at' => Carbon::now(),
+                'active' => true
+            ]);
+        }
+    }
+}
